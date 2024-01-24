d038beeff66ee627aa1dddf4bfc2c8edf693fc45
joelEVR <joel.velasquez.ca@gmail.com>
2024-01-24 02:21:35 -0500

    Re escritura commit daniel

--- assignment1W24/src/main/java/com/algonquin/cst8288/assignment1/controller/Rate.java
+++ assignment1W24/src/main/java/com/algonquin/cst8288/assignment1/controller/Rate.java
@@ -1,6 +1,6 @@
 package com.algonquin.cst8288.assignment1.controller;
 
-public class Rate {
+public interface Rate {
 	
 	public static final double BONUS_PERCENTAGE = 0.025;
 	public static final double PENSION_PERCENTAGE = 0.01;
--- assignment1W24/src/main/java/com/algonquin/cst8288/assignment1/emoloyee/EmployeePensionService.java
+++ assignment1W24/src/main/java/com/algonquin/cst8288/assignment1/emoloyee/EmployeePensionService.java
@@ -2,4 +2,7 @@ package com.algonquin.cst8288.assignment1.emoloyee;
 
 public interface EmployeePensionService {
 
+	public double pensionContribution(Employee employee);
+	
 }
+
