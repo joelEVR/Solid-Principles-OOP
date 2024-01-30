965ff104eb4ba2b6d5ac440140bcd366604ef69e
joelEVR <joel.velasquez.ca@gmail.com>
2024-01-29 21:07:01 -0500

    Comments for Rate.java

--- assignment1W24/src/main/java/com/algonquin/cst8288/assignment1/controller/Rate.java
+++ assignment1W24/src/main/java/com/algonquin/cst8288/assignment1/controller/Rate.java
@@ -1,8 +1,14 @@
 package com.algonquin.cst8288.assignment1.controller;
 
+/**
+ * The Rate class provides constants for calculating bonuses and pension contributions.
+ * It defines the percentage rates used throughout the application for these calculations.
+ */
 public class Rate {
-	
-	public static final double BONUS_PERCENTAGE = 0.025;
-	public static final double PENSION_PERCENTAGE = 0.01;
-}
+    
+    // Constant representing the bonus percentage.
+    public static final double BONUS_PERCENTAGE = 0.025;
 
+    // Constant representing the pension contribution percentage.
+    public static final double PENSION_PERCENTAGE = 0.01;
+}
